19b64fb45d2d3d7d9f405981b30cad457a6c44c7
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-07-27 15:50:52 -0300

    Add files via upload

---
+++ rafael_210727.py
@@ -0,0 +1,38 @@
+# Questions 2 e 3
+usa=["english","portuguese","spanish","france","japonese"]
+print(usa)
+# Question 4
+fire=usa[:6:2]
+for value in fire:
+    print (value)
+# Question 5
+eua=usa[:]
+# Question 6
+usa.append("chinese")
+# Question 7
+print(eua)
+# Question 8
+tuple=(10,200,300)
+# Question 9
+for value in tuple:
+    print(value)
+# Question 10
+for value in range(0,21):
+    print(value)
+# Question 11
+for value in range(0,21):
+    if value < 5 :
+        print(value)
+# Question 12 
+for value in range(0,21):
+    if value >= 14 :
+        print(value)
+# Question 13
+for value in range(0,21):
+    if value < 6 or value > 18 :
+        print(value)
+# Question 14
+for value in range(0,21):
+    if value < 6 or value > 18 :
+        print(value)
+#    else 
